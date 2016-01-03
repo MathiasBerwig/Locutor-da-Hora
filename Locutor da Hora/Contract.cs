b50bbd7a20888420012e4b0780524d74a233953d
Mathias Berwig <mathiasberwig@gmail.com>
2016-01-03 14:18:12 -0200

    Contract adicionado
    A classe define valores para campos comuns entre classes da aplicação e
    serve como referência para todos os usos destes campos.

---
+++ Locutor da Hora/Contract.cs
@@ -0,0 +1,9 @@
+﻿namespace Locutor_da_Hora
+{
+    /// <summary>
+    /// Define valores para campos comuns entre classes da aplicação. Serve como referência para todos os usos destes campos.
+    /// </summary>
+    public sealed class Contract
+    {
+    }
+}
